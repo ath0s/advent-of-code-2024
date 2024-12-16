98af78b4aebf95f4a4061012a98a52cdd72bd63e
Daniel Bergholm <861482+ath0s@users.noreply.github.com>
2024-12-16 19:28:52 +0100

    Update JUnit

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 dependencies {
     implementation(kotlin("reflect"))
 
-    testImplementation(platform("org.junit:junit-bom:5.11.3"))
+    testImplementation(platform("org.junit:junit-bom:5.11.4"))
     testImplementation("org.junit.jupiter:junit-jupiter-params")
     testImplementation(kotlin("test"))
 
